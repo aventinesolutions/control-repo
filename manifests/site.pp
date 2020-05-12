9f9a5d589f0728559e7dfd4cfabe08ac3d31fc9e
Matthew Eichler <matthew.eichler@aventinesolutions.nl>
2020-05-12 18:08:15 +0200

    :bear: :construction: remove the simple file example completely

--- manifests/site.pp
+++ manifests/site.pp
@@ -1,11 +1,2 @@
 node default {
-  file { '/root/README':
-    ensure  => file,
-    content => 'Aventine Solutions',
-    owner   => 'root',
-  }
-
-  file { '/root/README':
-    owner   => 'root',
-  }
 }
