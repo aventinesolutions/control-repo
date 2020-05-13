0504b859db5f3a1b2a3ff92ff2f7fc2e76690377
Matthew Eichler <matthew.eichler@aventinesolutions.nl>
2020-05-13 15:32:21 +0200

    :bear: :construction: fix our README file configurations

--- manifests/site.pp
+++ manifests/site.pp
@@ -1,15 +1,20 @@
 node default {
+  file { '/root/README':
+    ensure  => file,
+    content => "Aventine Solutions\n",
+    owner   => 'root',
+  }
 }
 node 'master.puppet.vm' {
   include role::master_server
-}
-
-node /^web/ {
-  include role::app_server
   file { '/root/README':
     ensure  => file,
-    content => $fqdn,
+    content => "Welcome to ${ fqdn } at Aventine Solutions\n",
+    owner   => 'root',
   }
+}
+node /^web/ {
+  include role::app_server
 }
 node /^db/ {
   include role::db_server
