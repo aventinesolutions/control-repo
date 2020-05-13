77610e9cbaddfc9d9d710fc1e79a41d74675b414
Matthew Eichler <matthew.eichler@aventinesolutions.nl>
2020-05-13 15:24:08 +0200

    :bear: :construction: use a "fact" to add content to a README file

--- manifests/site.pp
+++ manifests/site.pp
@@ -6,6 +6,10 @@ node 'master.puppet.vm' {
 
 node /^web/ {
   include role::app_server
+  file { '/root/README':
+    ensure  => file,
+    content => $fqdn,
+  }
 }
 node /^db/ {
   include role::db_server
