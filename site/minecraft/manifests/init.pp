17da3020325189c1f55c9aea4d0e7823346a0dbd
Matthew Eichler <matthew.eichler@aventinesolutions.nl>
2020-05-15 13:50:16 +0200

    :bear: :construction: try using "before" and "require" in our module definition

--- site/minecraft/manifests/init.pp
+++ site/minecraft/manifests/init.pp
@@ -5,6 +5,7 @@ class minecraft {
   file { '/opt/minecraft/server.jar':
     ensure => file,
     source => 'https://launcher.mojang.com/v1/objects/bb2b6b1aefcd70dfd1892149ac3a215f6c636b07/server.jar',
+    before => Service['minecraft'],
   }
   package { 'java':
     ensure => present,
@@ -18,7 +19,8 @@ class minecraft {
     source => 'puppet:///modules/minecraft/minecraft.service',
   }
   service { 'minecraft':
-    ensure => running,
-    enable => true,
+    ensure  => running,
+    enable  => true,
+    require => [Package['java'], File['/opt/minecraft/eula.txt'], File['/etc/systemd/system/minecraft.service']],
   }
 }
