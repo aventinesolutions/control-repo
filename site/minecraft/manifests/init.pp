82d8112fcf4824c3c5da2d1872601c527953c54b
Matthew Eichler <matthew.eichler@aventinesolutions.nl>
2020-05-15 14:11:58 +0200

    :bear: :construction: use the new template in the Minecraft module

--- site/minecraft/manifests/init.pp
+++ site/minecraft/manifests/init.pp
@@ -19,7 +19,9 @@ class minecraft (
   }
   file { '/etc/systemd/system/minecraft.service':
     ensure => file,
-    source => 'puppet:///modules/minecraft/minecraft.service',
+    content => epp('minecraft/minecraft.service', {
+      install_dir => $install_dir,
+    })
   }
   service { 'minecraft':
     ensure  => running,
