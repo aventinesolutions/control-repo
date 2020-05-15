c542da565938de032170a768820c256a25aa1f31
Matthew Eichler <matthew.eichler@aventinesolutions.nl>
2020-05-15 13:57:57 +0200

    :bear: :construction: using variables in the module to make code more maintainable

--- site/minecraft/manifests/init.pp
+++ site/minecraft/manifests/init.pp
@@ -1,16 +1,19 @@
 class minecraft {
-  file { '/opt/minecraft':
+  $url = 'https://launcher.mojang.com/v1/objects/bb2b6b1aefcd70dfd1892149ac3a215f6c636b07/server.jar'
+  $install_dir = '/opt/minecraft'
+
+  file { $install_dir:
     ensure => directory,
   }
-  file { '/opt/minecraft/server.jar':
+  file { "${install_dir}/server.jar":
     ensure => file,
-    source => 'https://launcher.mojang.com/v1/objects/bb2b6b1aefcd70dfd1892149ac3a215f6c636b07/server.jar',
+    source => $url,
     before => Service['minecraft'],
   }
   package { 'java':
     ensure => present,
   }
-  file { '/opt/minecraft/eula.txt':
+  file { "${install_dir}/eula.txt":
     ensure  => file,
     content => "eula=true\n",
   }
@@ -21,6 +24,6 @@ class minecraft {
   service { 'minecraft':
     ensure  => running,
     enable  => true,
-    require => [Package['java'], File['/opt/minecraft/eula.txt'], File['/etc/systemd/system/minecraft.service']],
+    require => [Package['java'], File["${install_dir}/eula.txt"], File['/etc/systemd/system/minecraft.service']],
   }
 }
