096b16c48dd252f885d4bd8a0c0a0466af6a049b
Matthew Eichler <matthew.eichler@aventinesolutions.nl>
2020-05-15 13:34:22 +0200

    :bear: :construction: fix a silly typo :bug:

--- site/minecraft/manifests/init.pp
+++ site/minecraft/manifests/init.pp
@@ -13,7 +13,7 @@ class minecraft {
     ensure  => file,
     content => "eula=true\n",
   }
-  file { '/etc/systemd/systems/minecraft.service':
+  file { '/etc/systemd/system/minecraft.service':
     ensure => file,
     source => 'puppet:///modules/minecraft/minecraft.service',
   }
