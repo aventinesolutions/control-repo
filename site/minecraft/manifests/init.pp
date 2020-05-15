991fb52e76777860a9a5680cf1ef12bacc27e7d5
Matthew Eichler <matthew.eichler@aventinesolutions.nl>
2020-05-15 14:00:29 +0200

    :bear: :construction: expose the two new variables for those using the module

--- site/minecraft/manifests/init.pp
+++ site/minecraft/manifests/init.pp
@@ -1,7 +1,7 @@
-class minecraft {
-  $url = 'https://launcher.mojang.com/v1/objects/bb2b6b1aefcd70dfd1892149ac3a215f6c636b07/server.jar'
+class minecraft (
+  $url         = 'https://launcher.mojang.com/v1/objects/bb2b6b1aefcd70dfd1892149ac3a215f6c636b07/server.jar',
   $install_dir = '/opt/minecraft'
-
+) {
   file { $install_dir:
     ensure => directory,
   }
